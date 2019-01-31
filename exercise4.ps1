b423dd51815e3640a597528ef951e77b7568a1a6
Rishab Behl <Rishab.Behl@appliedis.com>
2019-01-31 11:32:59 +0530

    powershell assignment on 31-01-2019

---
+++ exercise4.ps1
@@ -0,0 +1,25 @@
+﻿function Divide(){
+           
+           try{
+               [int]$result = $num4 / $num5
+               Write-host 'AAAANDDDD THE RESULTS IS:' $result
+            }
+      
+            catch [DivideByZeroException]{
+          
+                Write-Host " You can't divide by zero!!"
+                continue
+            }
+            catch{
+                    Write-Host "Error has occured"
+                    Write-Host $_.ErrorID
+                    Write-Host $_.Exception.Message
+                    break
+             }
+ 
+}  
+                [int]$num4 = Read-host 'Enter first number'
+                [int]$num5 = Read-host 'Enter second number'
+                Divide($num4,$num5)
+                
+               
