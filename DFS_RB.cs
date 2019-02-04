7aec5932ec443713464b530f003affca23cc4d2f
rishabbehl97 <47132214+rishabbehl97@users.noreply.github.com>
2019-02-04 13:56:05 +0530

    Add files via upload

---
+++ DFS_RB.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS.RB
+{
+        public class NodeStructureDFS                   // Structure of a new node created.
+        {
+            public int data;
+            public NodeStructureDFS left, right;
+            public NodeStructureDFS(int item)
+            {
+                data = item;
+                left = right = null;
+            }
+        }
+
+    public class DFS_RB
+    {
+        public NodeStructureDFS root;              // Root of the Binary Tree  
+
+        public DFS_RB()
+        {
+            root = null;
+        }
+
+
+        public void InorderTraversal(NodeStructureDFS node)  //Inorder traversal way of DFS
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            InorderTraversal(node.left);
+            Console.Write(node.data + " ");
+            InorderTraversal(node.right);
+        }
+
+
+        public void PreorderTraversal(NodeStructureDFS node)  //Preorder traversal way of DFS
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Console.Write(node.data + " ");
+            PreorderTraversal(node.left);
+            PreorderTraversal(node.right);
+        }
+
+
+        public void PostorderTraversal(NodeStructureDFS node) //Postorder traversal way of DFS
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            PostorderTraversal(node.left);
+            Console.Write(node.data + " ");
+            PostorderTraversal(node.right);
+        }
+
+        void printPostorder()
+        {
+            PostorderTraversal(root);
+        }
+
+        void printInorder()
+        {
+            InorderTraversal(root);
+        }
+
+        void printPreorder()
+        {
+            PreorderTraversal(root);
+        }
+
+        public static void Main(String[] args)
+        {
+            DFS_RB treeDFS = new DFS_RB();
+
+            treeDFS.root = new NodeStructureDFS(1);
+
+            treeDFS.root.left = new NodeStructureDFS(2);
+            treeDFS.root.right = new NodeStructureDFS(3);
+
+            treeDFS.root.left.left = new NodeStructureDFS(4);
+            treeDFS.root.left.right = new NodeStructureDFS(5);
+            treeDFS.root.right.left = new NodeStructureDFS(6);
+
+            Console.WriteLine("\n *******MENU*******"
+                                          + "\n 1. INORDER TRAVERSAL"
+                                          + "\n 2. PREORDER TRAVERSAL"
+                                          + "\n 3. POSTORDER TRAVERSAL"
+                                          + "\n 4. EXIT");
+            Console.WriteLine(" Enter the way in which you want to traverse and search for elemeent in the tree?");
+
+            string myChoiceDFS;
+            myChoiceDFS = Console.ReadLine();
+            int choiceDFS = Convert.ToInt32(myChoiceDFS);
+
+            switch (choiceDFS)
+            {
+                case 1: treeDFS.printInorder();
+                    break;
+                case 2: treeDFS.printInorder();
+                    break;
+                case 3: treeDFS.printPostorder();
+                    break;
+                case 4:
+                    break;
+                default: Console.WriteLine(" Wrong Choice. Better luck next time!!");
+                    break;
+            }
+            Console.Read();
+        }
+
+
+    }
+}
---
+++ StackRB.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS.RB
+{
+    class StackRB
+    {
+        static readonly int MAX_SV = 1000;      // Consider any random varible , to be infinity
+        int top;                                // Declaration of top element of stack.
+        int[] stack = new int[MAX_SV];
+
+        public bool IsEmpty()                   // It will return if stack is empty or not [Boolean function return 0(if empty) or 1]
+        {
+            return (top < 0);
+        }
+
+        public StackRB()                        // Constructor [Every time obj is created it'll initialize top to -1.
+        {
+            top = -1;
+        }
+
+        public bool PushInStack(int inputVal)       // Function to insert elements in the stack.
+        {
+            if (top >= MAX_SV)
+            {
+                Console.WriteLine("Stack has overflowed. Kindly adjust.");
+                return false;
+            }
+            else
+            {
+                stack[++top] = inputVal;
+                return true;
+            }
+        }
+
+        public int PopFromStack()                   // Function to remove one element from the stack
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow. Please fill something first in the stack.");
+                return 0;
+            }
+            else
+            {
+                int valueDeleted = stack[top--];
+                return valueDeleted;
+            }
+        }
+
+        public void PeekInStack()                   // Function to check the stack values.
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow. Please fill something in the stack.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(" Element at the top: {0}", stack[top]);
+            }
+        }
+
+        public void PrintStack()                  // Function to print the values of stack.
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow. Kindly fill something in the stack.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Elements of the stack are as:");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+        /*
+                public static void Main(String[] args)
+                {
+                    char answer = 'Y';
+                    StackRB Stackobj = new StackRB();
+                    while (answer == 'Y')
+                    {
+                        Console.WriteLine("\n Enter the operation, you want to perform in stack:" +
+                                          "\n *******MENU*******"
+                                          + "\n 1. PUSH"
+                                          + "\n 2. POP"
+                                          + "\n 3. PEEK"
+                                          + "\n 4. DISPLAY"
+                                          + "\n 5. EXIT");
+                        string myChoice;
+                        myChoice = Console.ReadLine();
+                        int choice = Convert.ToInt32(myChoice);
+
+                        switch (choice)
+                        {
+                            case 1:
+                                string inputVal;
+                                Console.WriteLine("Enter value you want to push into stack");
+                                inputVal = Console.ReadLine();
+                                int inputPush = Convert.ToInt32(inputVal);
+                                Stackobj.PushInStack(inputPush);
+                                break;
+                            case 2:
+                                Console.WriteLine("Value has been popped out! And value is : {0}" + Stackobj.PopFromStack());
+                                break;
+                            case 3:
+                                Stackobj.PopFromStack();
+                                break;
+                            case 4:
+                                Stackobj.PrintStack();
+                                break;
+                            case 5:
+                                return;
+                            default:
+                                Console.WriteLine("Wrong choice. Try it again!!");
+                                break;
+                        }
+
+                        Console.WriteLine(" Do you want to continue. If yes press Y and if no press N");
+                        String ans = Console.ReadLine();
+                        char ans_1 = Convert.ToChar(ans);
+
+                        if(ans_1 == 'Y')
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            answer = 'N';
+                        }
+                    }
+                        Console.Read();
+
+                }
+            }
+            */
+    }
+}
